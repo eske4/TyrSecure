a4b5a1edf84eefcd17137130fcc1a3b1d5d63f4e
eske4 <eske.larsen@gmail.com>
2025-12-11 12:37:04 +0100

    Reponsibility between cmake files fix

--- cmake/BPF.cmake
+++ cmake/BPF.cmake
@@ -3,16 +3,15 @@
 # Directory paths
 set(BPF_OUT ${CMAKE_BINARY_DIR}/bpfs)
 set(VMLINUX_OUT ${CMAKE_BINARY_DIR}/bpfs/vmlinux.h)
+set(SKEL_OUT_DIR ${CMAKE_BINARY_DIR}/skeletons)
 
 # Create output directories
 file(MAKE_DIRECTORY ${BPF_OUT})
 file(MAKE_DIRECTORY ${SKEL_OUT_DIR})
 
-
-
 execute_process(
-    COMMAND bpftool btf dump file /sys/kernel/btf/vmlinux format c
-    OUTPUT_FILE ${VMLINUX_OUT}
-    WORKING_DIRECTORY ${BPF_OUT}
-    RESULT_VARIABLE rv
-)
+  COMMAND bpftool btf dump file /sys/kernel/btf/vmlinux format c
+  OUTPUT_FILE ${VMLINUX_OUT}
+  WORKING_DIRECTORY ${BPF_OUT}
+  RESULT_VARIABLE rv)
+
