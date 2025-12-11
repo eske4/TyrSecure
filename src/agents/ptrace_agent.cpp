44d266772556a194477567b815dc43951bb20294
APaludan <andreashp@outlook.com>
2025-12-11 15:53:20 +0100

    Update ptrace_agent.cpp

--- src/agents/ptrace_agent.cpp
+++ src/agents/ptrace_agent.cpp
@@ -1,7 +1,7 @@
 #include "ptrace_agent.h"
 #include <cstdio>
 
-constexpr auto onEvent = [](ptrace_event e)
+constexpr auto on_event = [](ptrace_event e)
 { 
     printf("ptrace called by %s (PID %i), attaching to proc %i\n",
         e.caller_name,
@@ -10,7 +10,7 @@ constexpr auto onEvent = [](ptrace_event e)
 };
 
 ptrace_agent::ptrace_agent(pid_t protected_pid)
-    : handler(onEvent)
+    : handler(on_event)
 {
     handler.LoadAndAttachAll(protected_pid);
 }
